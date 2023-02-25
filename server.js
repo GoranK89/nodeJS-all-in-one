7f1da77459b347ce5148d902b719fc4ea0a4590b
Goran Kolar <86071804+GoranK89@users.noreply.github.com>
2023-02-25 11:34:52 +0100

    MVC

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
@@ -10,21 +11,6 @@ const PORT = process.env.PORT || 3500;
 app.use(logger);
 
 // cross origin resource sharing
-const whitelist = [
-  "https://www.yoursite.com",
-  "http://127.0.0.1:5500",
-  "http://localhost:3500",
-];
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.lastIndexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  optionsSuccessStatus: 200,
-};
 app.use(cors(corsOptions));
 
 // middleware to handle form data
@@ -35,11 +21,9 @@ app.use(express.json());
 
 //serve static files
 app.use(express.static(path.join(__dirname, "./public")));
-app.use("/subdir", express.static(path.join(__dirname, "./public")));
 
 // routes
 app.use("/", require("./routes/root"));
-app.use("/subdir", require("./routes/subdir"));
 app.use("/employees", require("./routes/api/employees"));
 
 // app.use('/')
